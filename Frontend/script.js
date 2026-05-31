cbb622fc2ae69c2666de81a08c1e737bd851dff8
mayasalazr <salazrmaya@gmail.com>
2026-05-31 11:22:44 -0600

    Front register

--- Frontend/script.js
+++ Frontend/script.js
@@ -17,3 +17,35 @@ function login(){
 
     alert("Login button works!");
 }
+
+function register(){
+
+    const username =
+        document.getElementById("username").value;
+
+    const email =
+        document.getElementById("email").value;
+
+    const password =
+        document.getElementById("password").value;
+
+    const confirmPassword =
+        document.getElementById("confirmPassword").value;
+
+    if(
+        username === "" ||
+        email === "" ||
+        password === "" ||
+        confirmPassword === ""
+    ){
+        alert("Please complete all fields");
+        return;
+    }
+
+    if(password !== confirmPassword){
+        alert("Passwords do not match");
+        return;
+    }
+
+    alert("Account created successfully!");
+}
