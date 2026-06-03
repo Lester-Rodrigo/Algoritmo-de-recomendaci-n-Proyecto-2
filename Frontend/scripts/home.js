bb00c7cb29e8484b501b5efcb8b6fc9f1d0324e9
MaFls-298 <marialefloress@outlook.com>
2026-06-03 16:07:59 -0600

    bug fixes

--- Frontend/scripts/home.js
+++ Frontend/scripts/home.js
@@ -2,7 +2,7 @@ let searchTimer;
 
 window.onload = () => {
     loadHero();
-    loadRecommended();
+    loadSections();
 };
 
 async function loadHero() {
@@ -17,7 +17,7 @@ async function loadHero() {
     const hero  = document.getElementById("hero");
 
     if (media.background || media.header_image) {
-        hero.style.backgroundImage = `linear-gradient(to right, rgba(7,11,29,.95) 15%, rgba(7,11,29,.0)), url('${media.background || media.header_image}')`;
+        hero.style.backgroundImage = `linear-gradient(to right, rgba(7,11,29,.95) 5%, rgba(7,11,29,.0)), url('${media.background || media.header_image}')`;
         hero.style.backgroundSize = "cover";
         hero.style.backgroundPosition = "center";
     }
@@ -30,17 +30,48 @@ async function loadHero() {
     document.getElementById("hero-like").onclick     = () => likeGame(heroGame.appid);
 }
 
-async function loadRecommended() {
-    let res   = await fetch(`${API}/recommendations/hybrid/${username}`);
-    let games = await res.json();
+async function loadSections() {
 
-    if (!games.length) {
-        res   = await fetch(`${API}/recommendations/genres/${username}`);
-        games = await res.json();
+    
+    let res = await fetch(`${API}/recommendations/hybrid/${username}`);
+    let games = await res.json();
+    renderGames(games, "recommended");
+
+    
+    const genres = await fetch(
+        `${API}/user/genres/${username}`
+    ).then(r => r.json());
+
+    const genreContainer = document.getElementById("genre-sections");
+
+    for (const genre of genres) {
+
+        const safeId = genre.replace(/\s+/g, "-");
+
+        const section = document.createElement("section");
+
+        section.innerHTML = `
+            <div class="section-title">${genre}</div>
+            <div class="games-grid" id="genre-${safeId}"></div>
+        `;
+
+        genreContainer.appendChild(section);
+
+        const genreGames = await fetch(
+            `${API}/games/genre/${encodeURIComponent(genre)}?limit=10`
+        ).then(r => r.json());
+
+        renderGames(genreGames, `genre-${safeId}`);
     }
 
-    renderGames(games, "recommended");
+    // Similar users
+    res = await fetch(`${API}/recommendations/collaborative/${username}`);
+    games = await res.json();
+    renderGames(games, "similarUsers");
 }
+
+
+
 
 function debounceSearch() {
     clearTimeout(searchTimer);
